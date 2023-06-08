4136d24c3b222c39047f014ff2edd570be33ada4
Vincent Guerin <ligne.68@gmail.com>
2023-06-08 17:08:48 +0200

    feat: add custom sign in stack.

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,29 @@
 import { withAuth } from 'next-auth/middleware';
 import { NextResponse } from 'next/server';
 
-import { MAINTENANCE_MODE } from './utils/app/const';
+import { MAINTENANCE_MODE } from '@/utils/app/const';
 
-export default withAuth(function middleware(req) {
-  if (MAINTENANCE_MODE) {
-    req.nextUrl.pathname = `/maintenance`;
-    return NextResponse.rewrite(req.nextUrl);
-  }
-  const username = req.nextauth.token?.email;
-  if (username) {
-    const requestHeaders = new Headers(req.headers);
-    requestHeaders.set('x-user', username);
-    return NextResponse.next({
-      request: {
-        headers: requestHeaders,
-      },
-    });
-  }
-});
+
+export default withAuth(
+  function middleware(req) {
+    if (MAINTENANCE_MODE) {
+      req.nextUrl.pathname = `/maintenance`;
+      return NextResponse.rewrite(req.nextUrl);
+    }
+    const username = req.nextauth.token?.email;
+    if (username) {
+      const requestHeaders = new Headers(req.headers);
+      requestHeaders.set('x-user', username);
+      return NextResponse.next({
+        request: {
+          headers: requestHeaders,
+        },
+      });
+    }
+  },
+  {
+    pages: {
+      signIn: '/auth/signin',
+    },
+  },
+);
